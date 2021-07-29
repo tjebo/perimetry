9fc2eb84c5f0815267415f06637376d100165399
tjebo <tjebn@web.de>
2021-07-29 17:37:49 +0100

    testthat get_json

--- tests/testthat/read_maia.R
+++ tests/testthat/read_maia.R
@@ -1,2 +1,15 @@
 library(perimetry)
 library(testthat)
+
+my_path <- file.path(getwd(), "test_data", "tgz_export")
+my_tgz <- show_tgz(my_path)
+unpack_tgz(my_tgz)
+my_tmp_dir <- create_ID_dirs(my_tgz)
+
+test_that("extract",{
+expect_warning(get_json(my_tmp_dir), regexp = NA)
+expect_equal(nrow(get_json(my_tmp_dir)), 6)
+}
+)
+
+# read_maia_tgz(my_path)
